6c13f967ffbd08445513dc1c06a6cd3daceeacf9
Nelson Feijo <nelson-feijo@hotmail.com>
2025-01-06 19:17:30 -0300

    Inicio do projeto

---
+++ frontend/santipizza/src/app/components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function Navbar() {
+    return ( 
+        <div className="flex ">
+            <div>image</div>
+            <div>Home</div>
+            <div>Monte a sua pizza</div>
+            <div>Combos</div>
+            <div>Bebidas</div>
+            <div>Contato</div>
+            <div>Botão</div>
+        </div>
+     );
+}
+
+export default Navbar;
---
+++ frontend/santipizza/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+import Navbar from "./components/layout/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <Navbar></Navbar>
+  );
+}
